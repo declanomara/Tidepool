660c324489bd41fe22534defc8b49de96a60a470
declan <25646795+declanomara@users.noreply.github.com>
2021-04-02 01:29:15 -0400

    Fixed typos in python script names

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ screen -XS streaming kill
 screen -XS dataprocessing kill
 
 echo "Starting data processing listener..."
-screen -dm -S 'dataprocessing' bash -c 'python3 processing.py; exec bash'
+screen -dm -S 'dataprocessing' bash -c 'python3 process.py; exec bash'
 
 echo "Starting data streaming script"
-screen -dmS 'streaming' bash -c 'python3 streaming.py; exec bash'
+screen -dmS 'streaming' bash -c 'python3 stream.py; exec bash'
 
 echo "Successfully started data collection"
