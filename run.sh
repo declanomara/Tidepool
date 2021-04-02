a4212d1d247f73a50d564d3a1384a6b7e8c28e3a
declan <25646795+declanomara@users.noreply.github.com>
2021-04-02 01:09:31 -0400

    Created startup script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Starting data processing listener..."
+screen -dmS 'dataprocessing' bash -c 'python3 processing.py'
+
+echo "Starting data streaming script"
+screen -dmS 'streaming' bash -c 'python3 streaming.py'
+
+echo "Successfully started data collection"
