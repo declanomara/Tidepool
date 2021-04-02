215ab122f4b9c83baf1da23c95e48f49b70a33af
declan <25646795+declanomara@users.noreply.github.com>
2021-04-02 01:23:14 -0400

    Prevented screens from closing after command is executed

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ screen -XS streaming kill
 screen -XS dataprocessing kill
 
 echo "Starting data processing listener..."
-screen -dmS 'dataprocessing' bash -c 'python3 processing.py'
+screen -dm -S 'dataprocessing' bash -c 'python3 processing.py; exec bash'
 
 echo "Starting data streaming script"
-screen -dmS 'streaming' bash -c 'python3 streaming.py'
+screen -dmS 'streaming' bash -c 'python3 streaming.py; exec bash'
 
 echo "Successfully started data collection"
