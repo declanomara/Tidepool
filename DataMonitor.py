92053930f83d54c379a3837bec701ac98810871a
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-21 12:30:29 -0700

    Updated DataMonitor to dynamically load database from config

--- DataMonitor.py
+++ DataMonitor.py
@@ -1,21 +1,26 @@
 import time
 import pprint
+from datetime import datetime
+
 import pymongo
+
+from helpers import load_config
 
 
 class DataMonitor:
     def __init__(self, db_string):
         self.db_string = db_string
         self.client = pymongo.MongoClient(db_string)
-        self.db = self.client["tidepool"]
+        self.data_db = self.client["tidepool"]
+        self.stats_db = self.client["tidepool-stats"]
 
     def get_instruments(self):
-        collections = self.db.list_collection_names()
+        collections = self.data_db.list_collection_names()
         collections.remove("raw")
         return collections
 
     def data_count(self, instrument):
-        collection = self.db[instrument]
+        collection = self.data_db[instrument]
         return collection.count_documents({})
 
     def total_data_count(self):
@@ -44,12 +49,24 @@ class DataMonitor:
         speeds = {instrument: gain[instrument] / time_span for instrument in gain}
         return speeds
 
+    def log_profile(self, profile):
+        latest = self.stats_db['latest']
+        historical = self.stats_db['historical']
+        timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        for key, value in profile.items():
+            stats = {'type': 'data_speed', 'instrument': key, 'data_rate': value, 'timestamp': timestamp}
+            latest.update_one({'instrument': key}, {'$set': stats}, upsert=True)
+            historical.insert_one(stats)
+
     def monitor(self):
         while True:
-            pprint.pprint(self.profile_total(1))
+            profile = self.profile_total(10)
+            self.log_profile(profile)
+            print(f'Profiled {len(profile.keys())} currency pairs.')
 
 
 if __name__ == "__main__":
-    db_string = "localhost:27017"
+    cfg = load_config()
+    db_string = cfg['db_string']
     dm = DataMonitor(db_string)
     dm.monitor()
