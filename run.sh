616b6327d17274d76565d5cde00347d22a2a4d75
declanomara <25646795+declanomara@users.noreply.github.com>
2022-04-21 13:06:24 -0700

    Fixed run script.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Kill any old screens
 killall screen
-
+sleep 1
 cd ~ && screen -dmS DataGatherer bash -c "cd Tidepool; python3 DataGatherer.py"
 cd ~ && screen -dmS DataMonitor bash -c "cd Tidepool; python3 DataMonitor.py"
